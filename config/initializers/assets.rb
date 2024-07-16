d937a9106c2a5942b87e4ea02137c42e39ff7e22
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-16 21:29:37 +0900

    PWA検証

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,6 +7,8 @@ Rails.application.config.assets.version = "1.0"
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
+Rails.application.config.assets.precompile += %w( manifest.json.erb )
+
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
