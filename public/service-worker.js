3c1785dc0799e2379172bbdbf26446afe58caf99
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-16 22:06:10 +0900

    PWA検証

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,17 +1,18 @@
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-  // キャッシュやその他のインストールタスクをここに書く
+const CACHE_NAME = 'example_cache';
+const urlsToCache = [
+  '/',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');
-  // 古いキャッシュのクリアなどのタスクをここに書く
-});
-
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
