35b3cc9a1f3aa05dfcc8aeedb559a8e2bbbca2aa
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 05:48:04 +0900

    web-push test

--- app/javascript/controllers/subscribe_controller.js
+++ app/javascript/controllers/subscribe_controller.js
@@ -1,3 +1,4 @@
+// app/javascript/controllers/subscribe_controller.js
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
@@ -5,8 +6,51 @@ export default class extends Controller {
     console.log("Subscribe controller connected")
   }
 
-  subscribe(event) {
+  async subscribe(event) {
     event.preventDefault()
-    console.log("Subscribed!")
+
+    if ("serviceWorker" in navigator) {
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js")
+
+        const permission = await Notification.requestPermission()
+        if (permission === "granted") {
+          const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: urlBase64ToUint8Array("BNFQW8D0D_iGfNze8OI_6vgrGIcLNusOHDIRPqs8ex1HAqtJhy1uKoe0-hnB-7THPHZjYGkVzDUQtQYYbgsbYgQ=")
+          })
+
+          // サーバーにサブスクリプション情報を送信する
+          await fetch("/subscriptions", {
+            method: "POST",
+            body: JSON.stringify(subscription),
+            headers: {
+              "Content-Type": "application/json"
+            }
+          })
+
+          console.log("Subscribed!")
+        } else {
+          console.log("Notification permission denied")
+        }
+      } catch (error) {
+        console.error("Service Worker registration or subscription failed", error)
+      }
+    } else {
+      console.log("Service Worker is not supported in this browser")
+    }
   }
+}
+
+// 公開鍵をUint8Arrayに変換する関数
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4)
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/")
+  const rawData = window.atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
 }
