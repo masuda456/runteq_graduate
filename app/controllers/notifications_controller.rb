5bb193caba97d60befafc7fda728cd90f4f7479d
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 04:03:50 +0900

    web push

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,22 +3,49 @@ class NotificationsController < ApplicationController
   protect_from_forgery except: [:subscribe]
 
   def subscribe
-    subscription = JSON.parse(request.body.read)
-    Webpush.payload_send(
-      message: {
-        title: 'Push Notification',
-        body: 'You have a new message!',
-        icon: '/images/icons/icon-192x192.png'
-      }.to_json,
-      endpoint: subscription['endpoint'],
-      p256dh: subscription['keys']['p256dh'],
-      auth: subscription['keys']['auth'],
-      vapid: {
-        subject: 'mailto:s.koike1989.hkbc07@gmail.com',
-        public_key: Rails.application.config.webpush[:vapid_key][:public_key],
-        private_key: Rails.application.config.webpush[:vapid_key][:private_key]
-      }
-    )
-    head :ok
+    begin
+      subscription = JSON.parse(request.body.read)
+      Webpush.payload_send(
+        message: {
+          title: 'Push Notification',
+          body: 'You have a new message!',
+          icon: '/images/icons/icon-192x192.png'
+        }.to_json,
+        endpoint: subscription['endpoint'],
+        p256dh: subscription['keys']['p256dh'],
+        auth: subscription['keys']['auth'],
+        vapid: {
+          subject: Rails.application.config.webpush[:vapid_key][:subject],
+          public_key: Rails.application.config.webpush[:vapid_key][:public_key],
+          private_key: Rails.application.config.webpush[:vapid_key][:private_key]
+        }
+      )
+      head :ok
+    rescue => e
+      Rails.logger.error "Error sending push notification: #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      render json: { error: e.message }, status: :internal_server_error
+    end
+  end
+
+  def self.send_daily_push_notifications
+    subscriptions = PushSubscription.all
+    subscriptions.each do |subscription|
+      Webpush.payload_send(
+        message: {
+          title: 'Daily Push Notification',
+          body: 'Here is your daily update!',
+          icon: '/images/icons/icon-192x192.png'
+        }.to_json,
+        endpoint: subscription.endpoint,
+        p256dh: subscription.p256dh,
+        auth: subscription.auth,
+        vapid: {
+          subject: Rails.application.config.webpush[:vapid_key][:subject],
+          public_key: Rails.application.config.webpush[:vapid_key][:public_key],
+          private_key: Rails.application.config.webpush[:vapid_key][:private_key]
+        }
+      )
+    end
   end
 end
--- config/initializers/webpush.rb
+++ config/initializers/webpush.rb
@@ -1,7 +1,7 @@
-Webpush.configure do |config|
-  config.vapid_key = {
+Rails.application.config.webpush = {
+  vapid_key: {
     vapid_public_key: 'BAD2vsi8pwGL0SLmyMUjhyaGAEXP0GKZT2exQp7f6EOlToqhCm9qDLNPPyg6TssGrOS_g6cSkAJ0zKQklOJL7LE',
     vapid_private_key: 'Jmo9OuGpelNpc5rWZnxhmtNdFtmO7gOI0tDDw6BThWA',
     vapid_subject: 'mailto:s.koike1989.hkbc07@gmail.com'
   }
-end
+}
