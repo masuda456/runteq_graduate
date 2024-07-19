b1007c1ca795da44ef9b9501880c222135647888
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 02:13:51 +0900

    PWA検証

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,16 +4,22 @@ class NotificationsController < ApplicationController
 
   def subscribe
     begin
-      subscription = JSON.parse(request.body.read)
+      subscription = {
+        endpoint: params[:endpoint],
+        keys: {
+          p256dh: params[:p256dh],
+          auth: params[:auth]
+        }
+      }
       Webpush.payload_send(
         message: {
           title: 'Push Notification',
           body: 'You have a new message!',
           icon: '/images/icons/icon-192x192.png'
         }.to_json,
-        endpoint: subscription['endpoint'],
-        p256dh: subscription['keys']['p256dh'],
-        auth: subscription['keys']['auth'],
+        endpoint: subscription[:endpoint],
+        p256dh: subscription[:keys][:p256dh],
+        auth: subscription[:keys][:auth],
         vapid: {
           subject: 'mailto:YOUR_EMAIL@example.com',
           public_key: Rails.application.config.webpush[:vapid_public_key],
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -46,31 +46,25 @@ function subscribeUserToPush(registration) {
   registration.pushManager.subscribe(subscribeOptions)
     .then(function(pushSubscription) {
       console.log('Received PushSubscription:', JSON.stringify(pushSubscription));
-      fetch('/notifications/subscribe', {
-        method: 'POST',
-        body: JSON.stringify(pushSubscription),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      }).then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      }).then(data => {
-        console.log('Subscription successful:', data);
-      }).catch(function(error) {
-        console.error('Failed to subscribe the user: ', error);
-        if (error.name) {
-          console.error('Error name:', error.name);
-        }
-        if (error.message) {
-          console.error('Error message:', error.message);
-        }
-        if (error.stack) {
-          console.error('Error stack:', error.stack);
-        }
+      
+      const query = new URLSearchParams({
+        endpoint: pushSubscription.endpoint,
+        p256dh: pushSubscription.keys.p256dh,
+        auth: pushSubscription.keys.auth
+      }).toString();
+      
+      return fetch(`/notifications/subscribe?${query}`, {
+        method: 'GET'
       });
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Subscription successful:', data);
     })
     .catch(function(error) {
       console.error('Failed to subscribe the user: ', error);
