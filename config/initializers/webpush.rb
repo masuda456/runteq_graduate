e5c6fbc6d22a20462b69bdd3bc70d9fed2e89fba
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 05:31:14 +0900

    web-pushで作り直し

--- config/initializers/webpush.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.config.webpush = {
-  vapid_key: {
-    vapid_public_key: 'BLLLR6hD2JNT-pSrRD6D3cexVqrnxoprMOQrb2CXWOpkyDlDNMKdIdSITWOy6hW2_7hgesvF2U76RW4s2bLoVMw',
-    vapid_private_key: 'oRX8_JsHTvFXFFvVp51fGNXPJCIJsmT86UKgbFhoEaI',
-    vapid_subject: 'mailto:s.koike1989.hkbc07@gmail.com'
-  }
-}
