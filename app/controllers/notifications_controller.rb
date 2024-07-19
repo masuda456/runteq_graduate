551824d00d73d9bcbea37c08e0fac71f64b08312
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 03:00:04 +0900

    web push post

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,22 +4,16 @@ class NotificationsController < ApplicationController
 
   def subscribe
     begin
-      subscription = {
-        endpoint: params[:endpoint],
-        keys: {
-          p256dh: params[:p256dh],
-          auth: params[:auth]
-        }
-      }
+      subscription = JSON.parse(request.body.read)
       Webpush.payload_send(
         message: {
           title: 'Push Notification',
           body: 'You have a new message!',
           icon: '/images/icons/icon-192x192.png'
         }.to_json,
-        endpoint: subscription[:endpoint],
-        p256dh: subscription[:keys][:p256dh],
-        auth: subscription[:keys][:auth],
+        endpoint: subscription['endpoint'],
+        p256dh: subscription['keys']['p256dh'],
+        auth: subscription['keys']['auth'],
         vapid: {
           subject: 'mailto:YOUR_EMAIL@example.com',
           public_key: Rails.application.config.webpush[:vapid_public_key],
