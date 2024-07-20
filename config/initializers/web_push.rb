7c556c844ecd0209e3794933f91d179ebcaa1628
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-21 02:23:31 +0900

    web push

--- config/initializers/web_push.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/web_push.rb
-WebPush.configure do |config|
-  config.vapid_key = {
-    subject: Rails.application.credentials.dig(:webpush, :vapid_key, :subject),
-    public_key: Rails.application.credentials.dig(:webpush, :vapid_key, :public_key),
-    private_key: Rails.application.credentials.dig(:webpush, :vapid_key, :private_key)
-  }
-end
