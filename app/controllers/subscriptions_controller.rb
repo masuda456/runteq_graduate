ca5ccec218d9de2156d3e4657a4558b983050c74
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-21 01:55:26 +0900

    修正

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,36 +1,24 @@
 class SubscriptionsController < ApplicationController
-  protect_from_forgery except: :create
-
   def create
     subscription = Subscription.new(subscription_params)
     subscription.user = current_user
 
     if subscription.save
-      begin
-        Rails.logger.info "Attempting to send push notification to #{subscription.endpoint}"
-
-        Webpush.payload_send(
-          message: {
-            title: 'Subscription Saved',
-            body: 'You have a new subscription'
-          }.to_json,
-          endpoint: subscription.endpoint,
-          p256dh: subscription.p256dh_key,
-          auth: subscription.auth_key,
-          vapid: {
-            subject: Rails.application.credentials.dig(:webpush, :vapid_key, :subject),
-            public_key: Rails.application.credentials.dig(:webpush, :vapid_key, :public_key),
-            private_key: Rails.application.credentials.dig(:webpush, :vapid_key, :private_key)
-          }
-        )
-
-        Rails.logger.info "Push notification sent successfully"
-        render json: { status: :ok }
-      rescue => e
-        Rails.logger.error "Error sending push notification: #{e.message}"
-        Rails.logger.error e.backtrace.join("\n")
-        render json: { status: :internal_server_error, error: e.message }
-      end
+      WebPush.payload_send(
+        message: {
+          title: 'Success save ',
+          body: 'You have a new subscription'
+        }.to_json,
+        endpoint: subscription.endpoint,
+        p256dh: subscription.p256dh_key,
+        auth: subscription.auth_key,
+        vapid: {
+          subject: Rails.application.credentials.dig(:webpush, :vapid_key, :subject),
+          public_key: Rails.application.credentials.dig(:webpush, :vapid_key, :public_key),
+          private_key: Rails.application.credentials.dig(:webpush, :vapid_key, :private_key)
+        }
+      )
+      render json: { status: :ok }
     else
       Rails.logger.error subscription.errors.full_messages
       render json: { status: :unprocessable_entity, errors: subscription.errors.full_messages }
@@ -40,6 +28,6 @@ class SubscriptionsController < ApplicationController
   private
 
   def subscription_params
-    params.require(:subscription).permit(:endpoint, :auth_key, :p256dh_key)
+    params.require(:subscription).permit(:endpoint, :p256dh_key, :auth_key)
   end
 end
--- config/initializers/web-push.rb
+++ config/initializers/web-push.rb
@@ -1,4 +1,4 @@
-Webpush.configure do |config|
+WebPush.configure do |config|
   config.vapid_key = {
     subject: Rails.application.credentials.dig(:webpush, :vapid_key, :subject),
     public_key: Rails.application.credentials.dig(:webpush, :vapid_key, :public_key),
