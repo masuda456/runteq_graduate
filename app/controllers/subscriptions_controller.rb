d0e81d2f265dadc150d218f969b30543f0872235
rq-masuda456-at-321899771753 <s.koike1989.hkbc07@gmail.com>
2024-07-20 14:08:18 +0900

    web push 実験

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,49 +1,45 @@
 class SubscriptionsController < ApplicationController
-  # POST /subscriptions or /subscriptions.json
+  protect_from_forgery except: :create
+
   def create
     subscription = Subscription.new(subscription_params)
     subscription.user = current_user
 
     if subscription.save
-      WebPush.payload_send(
-        message: {
-          title: 'success save ',
-          body: 'You have a new subscription'
-        }.to_json,
-        endpoint: subscription['endpoint'],
-        p256dh: subscription['keys']['p256dh'],
-        auth: subscription['keys']['auth'],
-        vapid: {
-          subject: Rails.application.config.webpush[:vapid_key][:subject],
-          public_key: Rails.application.config.webpush[:vapid_key][:public_key],
-          private_key: Rails.application.config.webpush[:vapid_key][:private_key]
-        }
-      )
-      render json: { status: :ok }
+      begin
+        Rails.logger.info "Attempting to send push notification to #{subscription.endpoint}"
+
+        WebPush.payload_send(
+          message: {
+            title: 'success save subscription',
+            body: 'You have a new subscription'
+          }.to_json,
+          endpoint: subscription.endpoint,
+          p256dh: subscription.keys['p256dh'],
+          auth: subscription.keys['auth'],
+          vapid: {
+            subject: Rails.application.config.webpush[:vapid_key][:subject],
+            public_key: Rails.application.config.webpush[:vapid_key][:public_key],
+            private_key: Rails.application.config.webpush[:vapid_key][:private_key]
+          }
+        )
+
+        Rails.logger.info "Push notification sent successfully"
+        render json: { status: :ok }
+      rescue => e
+        Rails.logger.error "Error sending push notification: #{e.message}"
+        Rails.logger.error e.backtrace.join("\n")
+        render json: { status: :internal_server_error, error: e.message }
+      end
     else
       Rails.logger.error subscription.errors.full_messages
       render json: { status: :unprocessable_entity, errors: subscription.errors.full_messages }
     end
   end
 
-  # DELETE /subscriptions or /subscriptions.json
-  def destroy
-    subscription = Subscription.find_by(
-      endpoint: subscription_params[:endpoint],
-      user_id: subscription_params[:user_id]&.to_i
-    )
-
-    if subscription.destroy
-      render json: { status: :ok }
-    else
-      render json: { status: :unprocessable_entity }
-    end
-  end
-
   private
-    def subscription_params
-      params
-	.require(:subscription)
-	.permit(:endpoint, :auth_key, :p256dh_key, :user_id)
-    end
+
+  def subscription_params
+    params.require(:subscription).permit(:endpoint, keys: [:p256dh, :auth])
+  end
 end
